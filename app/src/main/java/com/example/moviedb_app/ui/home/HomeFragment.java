e3f1155057d4006faea9edff5aba127b2e8e3dbc
Alex <lin.alexandre.16@gmail.com>
2020-03-13 10:41:19 +0100

    First try to implement search function

--- app/src/main/java/com/example/moviedb_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/moviedb_app/ui/home/HomeFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -17,6 +20,7 @@ import com.example.moviedb_app.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    public static MutableLiveData<String> test;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +28,15 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+        final EditText search_input = root.findViewById(R.id.search_input);
+        Button search_button =root.findViewById(R.id.search_button);
+
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(search_input.getText().toString());
+            }
+        });
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
