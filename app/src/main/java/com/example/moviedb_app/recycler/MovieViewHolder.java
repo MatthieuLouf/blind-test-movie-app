94558a35e3720290f7078248e139786276cee5cd
matthieu.louf <matthieu.louf@edu.devinci.fr>
2020-03-20 23:13:36 +0100

    add image loading

--- app/src/main/java/com/example/moviedb_app/recycler/MovieViewHolder.java
+++ app/src/main/java/com/example/moviedb_app/recycler/MovieViewHolder.java
@@ -1,20 +1,28 @@
 package com.example.moviedb_app.recycler;
 
 
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.moviedb_app.R;
 import com.example.moviedb_app.model.Movie;
 import com.example.moviedb_app.ui.detail_movie_activity.MovieDetailsActivity;
 
 public class MovieViewHolder extends RecyclerView.ViewHolder {
 
+    private ProgressBar progressBar;
     private ImageView image;
     private TextView original_title;
     private TextView rate;
@@ -26,6 +34,7 @@ public class MovieViewHolder extends RecyclerView.ViewHolder {
 
     public MovieViewHolder(@NonNull View itemView, String viewType) {
         super(itemView);
+        this.progressBar = itemView.findViewById(R.id.progress_circular);
         this.image = itemView.findViewById(R.id.image);
         this.original_title = itemView.findViewById(R.id.original_title);
         this.rate = itemView.findViewById(R.id.rating);
@@ -38,7 +47,19 @@ public class MovieViewHolder extends RecyclerView.ViewHolder {
 
     public void bind(final Movie movie) {
         original_title.setText(movie.getTitle());
-        Glide.with(itemView).load(BASE_URL_IMAGE + movie.getPosterPath()).into(image);
+        progressBar.setVisibility(View.VISIBLE);
+        Glide.with(itemView).load(BASE_URL_IMAGE + movie.getPosterPath()).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                progressBar.setVisibility(View.GONE);
+                return false;
+            }
+        }).into(image);
 
         if (this.viewType.equals("vertical_view")) {
             rate.setText("Rating : " + movie.getVoteAverage().toString());
