36e23e0128d166e38ca61b8e96e38cec39177b1f
matthieu.louf <matthieu.louf@edu.devinci.fr>
2020-04-09 01:25:50 +0200

    searched movie card

--- app/src/main/java/com/example/moviedb_app/ui/blindtest/OneMovieFragment.java
+++ app/src/main/java/com/example/moviedb_app/ui/blindtest/OneMovieFragment.java
@@ -3,6 +3,7 @@ package com.example.moviedb_app.ui.blindtest;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
@@ -10,10 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.moviedb_app.R;
 import com.example.moviedb_app.model.Movie;
 import com.example.moviedb_app.model.MoviePageResult;
@@ -21,6 +24,7 @@ import com.example.moviedb_app.model.Video;
 import com.example.moviedb_app.model.VideoPageResult;
 import com.example.moviedb_app.network.GetMovieService;
 import com.example.moviedb_app.network.RetrofitInstance;
+import com.example.moviedb_app.ui.detail_movie_activity.MovieDetailsActivity;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
@@ -42,7 +46,7 @@ import retrofit2.Retrofit;
  * create an instance of this fragment.
  */
 public class OneMovieFragment extends Fragment {
-
+    private String BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/";
     private static final String ARG_PARAM1 = "movie_id";
 
     private Integer movie_id;
@@ -53,13 +57,14 @@ public class OneMovieFragment extends Fragment {
     private NumberPicker picker;
     private TextView movie_title;
     private YouTubePlayerView youTubePlayerView;
+    private CardView movieCardView;
 
     private boolean next = false;
     private List<String> listSimilarTitles = new ArrayList<String>();
 
-
-    Retrofit retrofit = RetrofitInstance.getRetrofitInstance();
-    GetMovieService retrofitService = retrofit.create(GetMovieService.class);
+    private View root;
+    private Retrofit retrofit = RetrofitInstance.getRetrofitInstance();
+    private GetMovieService retrofitService = retrofit.create(GetMovieService.class);
 
     public OneMovieFragment() {
     }
@@ -83,25 +88,24 @@ public class OneMovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_one_movie, container, false);
+        root = inflater.inflate(R.layout.fragment_one_movie, container, false);
 
-        movie_title = root.findViewById(R.id.one_movie_title);
+        movie_title = root.findViewById(R.id.movie_title);
         hider_top = root.findViewById(R.id.hider_top);
         next_movie = root.findViewById(R.id.next_movie);
         picker = root.findViewById(R.id.movie_picker);
         picker.setVisibility(View.INVISIBLE);
         picker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+        movieCardView = root.findViewById(R.id.card_movie_view);
+        movieCardView.setVisibility(View.INVISIBLE);
 
         next_movie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!next) {
-                    movie_title.setText(searched_movie.getTitle());
-                    next = true;
-                    next_movie.setText(R.string.next_movie);
+                    showResult();
                 } else {
-                    BlindtestMovieActivity blindtestMovieActivity = (BlindtestMovieActivity) getActivity();
-                    blindtestMovieActivity.getRandomMovie();
+                    changeFragment();
                 }
             }
         });
@@ -124,6 +128,7 @@ public class OneMovieFragment extends Fragment {
                 searched_movie = response.body();
                 getBestTrailer(searched_movie.getId().toString());
                 setSimilarMovies();
+                setMovieViewComponents();
             }
 
             @Override
@@ -174,16 +179,16 @@ public class OneMovieFragment extends Fragment {
             public void onVideoId(@NonNull YouTubePlayer youTubePlayer, String videoId) {
 
             }
+
             @Override
-            public void onError(@NonNull YouTubePlayer youTubePlayer, @NonNull PlayerConstants.PlayerError error)
-            {
-                Toast.makeText(getActivity(), "video_error", Toast.LENGTH_LONG).show();
+            public void onError(@NonNull YouTubePlayer youTubePlayer, @NonNull PlayerConstants.PlayerError error) {
+                changeFragment();
             }
         });
     }
 
     public void getBestTrailer(String movie_id) {
-        retrofitService.getVideos(movie_id,  getString(R.string.tmdb_api_key), getString(R.string.api_language_key)).enqueue(new Callback<VideoPageResult>() {
+        retrofitService.getVideos(movie_id, getString(R.string.tmdb_api_key), getString(R.string.api_language_key)).enqueue(new Callback<VideoPageResult>() {
             @Override
             public void onResponse(Call<VideoPageResult> call, Response<VideoPageResult> response) {
                 if (response.body() != null) {
@@ -192,8 +197,7 @@ public class OneMovieFragment extends Fragment {
                     if (video != null) {
                         initVideo(video.getKey());
                     } else {
-                        BlindtestMovieActivity blindtestMovieActivity = (BlindtestMovieActivity) getActivity();
-                        blindtestMovieActivity.getRandomMovie();
+                        changeFragment();
                     }
                 }
             }
@@ -212,8 +216,7 @@ public class OneMovieFragment extends Fragment {
                     if (video.getName().toLowerCase().contains("vost")) {
                         videoSelected = video;
                         break;
-                    }
-                    else{
+                    } else {
                         videoSelected = video;
                     }
                 } else {
@@ -225,28 +228,24 @@ public class OneMovieFragment extends Fragment {
         return videoSelected;
     }
 
-    private void setSimilarMovies()
-    {
-        retrofitService.getSimilarMovies(movie_id.toString(),1, getString(R.string.tmdb_api_key),getString(R.string.api_language_key)).enqueue(new Callback<MoviePageResult>() {
+    private void setSimilarMovies() {
+        retrofitService.getSimilarMovies(movie_id.toString(), 1, getString(R.string.tmdb_api_key), getString(R.string.api_language_key)).enqueue(new Callback<MoviePageResult>() {
             @Override
             public void onResponse(Call<MoviePageResult> call, Response<MoviePageResult> response) {
                 MoviePageResult moviePageResult = response.body();
-                if(moviePageResult!=null)
-                {
+                if (moviePageResult != null) {
                     List<Movie> movieList = moviePageResult.getResults();
-                    for(Movie movie : movieList)
-                    {
-                        if(movie.getTitle()!=null && !movie.getTitle().replaceAll("[^a-zA-Z0-9]", "").equals(""))
-                        {
+                    for (Movie movie : movieList) {
+                        if (movie.getTitle() != null && !movie.getTitle().replaceAll("[^a-zA-Z0-9]", "").equals("")) {
                             listSimilarTitles.add(movie.getTitle());
                         }
                     }
                     listSimilarTitles.add(searched_movie.getTitle());
                     Collections.sort(listSimilarTitles);
                     picker.setMinValue(0);
-                    picker.setMaxValue(listSimilarTitles.size()-1);
-                    picker.setDisplayedValues( listSimilarTitles.toArray(new String[listSimilarTitles.size()]));
-                    picker.setValue(listSimilarTitles.size()/2);
+                    picker.setMaxValue(listSimilarTitles.size() - 1);
+                    picker.setDisplayedValues(listSimilarTitles.toArray(new String[listSimilarTitles.size()]));
+                    picker.setValue(listSimilarTitles.size() / 2);
                     picker.setVisibility(View.VISIBLE);
                 }
             }
@@ -262,5 +261,32 @@ public class OneMovieFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         youTubePlayerView.release();
+    }
+
+    private void changeFragment() {
+        BlindtestMovieActivity blindtestMovieActivity = (BlindtestMovieActivity) getActivity();
+        blindtestMovieActivity.getRandomMovie();
+    }
+
+    private void showResult() {
+        next = true;
+        next_movie.setText(R.string.next_movie);
+        next_movie.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        movieCardView.setVisibility(View.VISIBLE);
+        picker.setVisibility(View.INVISIBLE);
+    }
+
+    private void setMovieViewComponents()
+    {
+        ImageView movie_image = root.findViewById(R.id.movie_image);
+        TextView movie_rating = root.findViewById(R.id.movie_rating);
+        TextView movie_release_date = root.findViewById(R.id.movie_release_date);
+        TextView movie_language = root.findViewById(R.id.movie_language);
+
+        Glide.with(this).load(BASE_URL_IMAGE + searched_movie.getPosterPath()).into(movie_image);
+        movie_title.setText(searched_movie.getTitle());
+        movie_rating.setText(getString(R.string.average_rate)+" : "+searched_movie.getVoteAverage().toString()+"/10");
+        movie_release_date.setText(getString(R.string.release_date)+" : "+searched_movie.getReleaseDate().replace('-', '/'));
+        movie_language.setText(getString(R.string.language_is)+" : "+searched_movie.getOriginalLanguage().toUpperCase());
     }
 }
