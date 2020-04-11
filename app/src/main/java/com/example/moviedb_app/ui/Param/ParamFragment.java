e86e3b983f147a4fa4acbb094ca27b87e65d7767
matthieu.louf <matthieu.louf@edu.devinci.fr>
2020-04-11 18:50:33 +0200

    reformat top -> personalized test

---
+++ app/src/main/java/com/example/moviedb_app/ui/Param/ParamFragment.java
@@ -0,0 +1,24 @@
+package com.example.moviedb_app.ui.Param;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.moviedb_app.R;
+
+public class ParamFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_param, container, false);
+
+        return root;
+    }
+}
+
+
